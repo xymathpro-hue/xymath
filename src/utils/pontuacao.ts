f8a05148c753c0e302ae1e41875d3a82efdbd222
xymathpro-hue <xymathpro@gmail.com>
2026-01-23 17:05:13 -0300

    Update pontuacao.ts

--- src/utils/pontuacao.ts
+++ src/utils/pontuacao.ts
@@ -1,1 +1,17 @@
+export function calcularPontuacao(
+  valorTotal: number,
+  totalQuestoes: number
+) {
+  if (totalQuestoes <= 0) {
+    return {
+      valorPorQuestao: 0,
+      valorTotal: valorTotal
+    }
+  }
+
+  return {
+    valorPorQuestao: Number((valorTotal / totalQuestoes).toFixed(2)),
+    valorTotal: Number(valorTotal.toFixed(1))
+  }
+}
 
