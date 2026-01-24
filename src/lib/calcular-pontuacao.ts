4f8ebab02b4e94462b8456369a28335e8280a096
xymathpro-hue <xymathpro@gmail.com>
2026-01-24 09:33:59 -0300

    Update calcular-pontuacao.ts

--- src/lib/calcular-pontuacao.ts
+++ src/lib/calcular-pontuacao.ts
@@ -1,1 +1,31 @@
+interface CalcularPontuacaoParams {
+  valorTotal: number
+  totalQuestoes: number
+  acertos?: number
+}
+
+export function calcularPontuacao({
+  valorTotal,
+  totalQuestoes,
+  acertos
+}: CalcularPontuacaoParams) {
+  if (totalQuestoes === 0) {
+    return {
+      valorPorQuestao: 0,
+      notaFinal: 0
+    }
+  }
+
+  const valorPorQuestao = valorTotal / totalQuestoes
+
+  const notaFinal =
+    typeof acertos === 'number'
+      ? Number((acertos * valorPorQuestao).toFixed(2))
+      : 0
+
+  return {
+    valorPorQuestao: Number(valorPorQuestao.toFixed(2)),
+    notaFinal
+  }
+}
 
